35bea09c851d8cfa7f142578bdacd6b076504d7b
nicotrejos <56837715+nicotrejos@users.noreply.github.com>
2019-12-06 10:17:38 -0600

    12 commit

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ const validations = {
 		return value.match(/^([1-9][0-9]{0,2})$/);
 	}
 
-	/*countryCode: function(value) {
+	countryCode: function(value) {
 		return value.length === 2 && isNaN(value);
-	}*/
+	}
 };
 
 // Validation error messages
